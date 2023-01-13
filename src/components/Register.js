49208e1d6e8d96403fe95588c41b6e70e24c9ac0
Lukow4 <luke.kowieski@gmail.com>
2023-01-13 10:30:35 +0100

    90% of singup site is ready

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import HomeNavi from "./HomeNavi";
 import HomeHeader from "./HomeHeader";
+import './register.scss';
 
 const Register = () => {
     return (
         <>
             <HomeHeader/>
-        <div>
-           Rejestracja
+        <div className='register'>
+           <h2>Załóż konto</h2>
+            <form>
+                <div className='form-fields'>
+                    <div className='single-field'>
+                        <label htmlFor="">Email</label>
+                        <input type="text" name="Email"></input>
+                    </div>
+                    <div className='single-field'>
+                        <label htmlFor="">Hasło</label>
+                        <input type="password" name="Hasło"></input>
+                    </div>
+                    <div className='single-field'>
+                        <label htmlFor="">Powtórz hasło</label>
+                        <input type="password" name="Powtórz hasło"></input>
+                    </div>
+                    <div className='form-buttons'>
+                    </div>
+                </div>
+                <div className='form-buttons'>
+                    <button className='empty-button'>Zaloguj się</button>
+                    <button>Załóż konto</button>
+                </div>
+            </form>
         </div>
         </>
     );
