6b1dc542405e42ec7246b799eee7235b0ac05221
Lukow4 <luke.kowieski@gmail.com>
2023-01-19 10:17:47 +0100

    start home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import HomeHeader from "./HomeHeader";
+import '../scss/home.scss';
+import {Link as ScrollLink} from "react-scroll";
+
 
 function Contact() {
     return <div style={{marginTop: "1000px"}} id="contact">Kontakt</div>
@@ -9,10 +12,37 @@ const Home = () => {
     return (
         <>
             <HomeHeader/>
-            <div>
-                Home
-            </div>
-            <Contact />
+            <section className='top-block'>
+                <div className='hero-photo'></div>
+                <div className='hero-text'>
+                    <h2>Zacznij pomagać!</h2>
+                    <h2>Oddaj niechciane rzeczy w zaufane ręce</h2>
+                    <div className='decoration'/>
+                    <div className='bttns'>
+                        <ScrollLink
+                            activeClass="active"
+                            to=""
+                            spy={true}
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                        >
+                            ODDAJ<br/>RZECZY
+                        </ScrollLink>
+                        <ScrollLink
+                            activeClass="active"
+                            to=""
+                            spy={true}
+                            smooth={true}
+                            offset={50}
+                            duration={500}
+                        >
+                            ZORGANIZUJ<br/>ZBIÓRKĘ
+                        </ScrollLink>
+
+                    </div>
+                </div>
+        </section>
         </>
     );
 };
