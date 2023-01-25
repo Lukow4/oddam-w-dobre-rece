b33feb84a3028c20b05afc240b495ef1461bd749
Lukow4 <luke.kowieski@gmail.com>
2023-01-25 12:20:46 +0100

    form logic is done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,78 +1,128 @@
+import React, {Component} from 'react';
 import '../scss/contact.scss';
-import React, {useState} from 'react';
 
 
-const Contact = () => {
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        message: ""
-    });
+class HomeContact extends Component{
+    state={
+        name:'',
+        email:'',
+        message:'',
 
-    const inputHandler = (e) => {
-        setFormData((prev) => {
-            return {
-                ...prev,
-                [e.target.name]: e.target.value
-            }
+        nameError:false,
+        emailError:false,
+        messageError:false,
+
+        messageSend:false,
+        fetchError:false
+
+    };
+    handleChange = e =>{
+        this.setState({
+            [e.target.name]:e.target.value
         })
-    }
+    };
+    sendForm=(name,email,message)=>{
+        fetch("https://fer-api.coderslab.pl/v1/portfolio/contact", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name, email, message
+            })
+        })
+            .then((data)=> {
+                console.log(data);
+                this.setState({messageSend:true,fetchError:false})
+            })
+            .catch((error)=> {
+                console.log(error);
+                this.setState({messageSend:false,fetchError:true});
+            })
+    };
 
-    const submitHandler = (e) => {
+    handleSubmit=(e)=>{
         e.preventDefault();
-        fetch('https://fer-api.coderslab.pl/v1/portfolio/contact', {
-           method: "Post",
-           headers: {"Content-Type": "application/json"},
-           body: JSON.stringify(formData)
-        })
-            .then(res => res.json())
-            .then(res2 => console.log(res2))
-            .catch(error => console.log({error}))
+        this.setState({messageSend:false});
+        let name=this.state.name;
+        let email=this.state.email;
+        let message=this.state.message;
+        let correctMessage=true;
+
+        let nameRegex=/^[a-zA-Z]{1,200}$/;
+
+        let emailRegex= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        let messageRegex=/^.{120,1000}$/;
+
+        console.log('Regex mail',emailRegex.test(email));
+        console.log('Reg Name',nameRegex.test(name));
+        console.log("reg mess", messageRegex.test(message));
+
+        if (!nameRegex.test(name)){
+            this.setState({nameError:true});
+            correctMessage=false;
+        }else{
+            this.setState({nameError:false});
+        }
+
+        if (!emailRegex.test(email)){
+            this.setState({emailError:true});
+            correctMessage=false;
+        }else{
+            this.setState({emailError:false});
+        }
+
+        if (!messageRegex.test(message)){
+            this.setState({messageError:true});
+            correctMessage=false;
+        }else{
+            this.setState({messageError:false});
+        }
+
+        if (correctMessage){
+            this.sendForm(name,email,message);
+        }
+
+
+
+    };
+    render() {
+        return(
+            <div className='contact' name="homeContact">
+                <form onSubmit={this.handleSubmit}>
+                    <h2>Skontaktuj się z nami</h2>
+                    <div className='decoration'/>
+                    <div className='messageSend'>
+                        {this.state.messageSend && <p className='send'>Wiadomość została wysłana!<br/>Wkrótce się skonatktujemy.</p>}
+                        {this.state.fetchError && <p className='error'>Bład serwera!</p>}
+                        {(!this.state.messageSend && !this.state.fetchError) && <p/>}
+
+
+                    </div>
+                    <div className='nameAndEmail'>
+                        <div className='singleField'>
+                            <label htmlFor='contactName'>Wpisz swoje imię</label>
+                            <input type="text"  placeholder='Krzysztof' value={this.state.name} name='name' onChange={this.handleChange}/>
+                            {(this.state.nameError)?<span>Podane imię jest nieprawidłowe!</span>:<span className='correct'/>}
+                        </div>
+                        <div className='singleField'>
+                            <label htmlFor='contactMail'>Wpisz swój email</label>
+                            <input type="text" placeholder='abc@xyz.pl' value={this.state.email} name='email' onChange={this.handleChange}/>
+                            {(this.state.emailError)?<span>Podane email jest nieprawidłowy!</span>:<span className='correct'/>}
+                        </div>
+                    </div>
+                    <label htmlFor='contactMessage'>Wpisz swoja wiadomość</label>
+                    <textarea rows="4" placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet nibh massa. Nullam blandit, felis sed tincidunt porta, mi urna facilisis est, eu varius leo tortor sit amet ex. Nulla et ullamcorper felis, dictum molestie mi.' value={this.state.message} name='message' onChange={this.handleChange}/>
+                    {(this.state.messageError)?<span>Widomość musi miec  conajmniej 120 znaków!</span>:<span className='correct'/>}
+                    <div className='submit'>
+                        <input type='submit' value="Wyślij"/>
+                    </div>
+
+                </form>
+            </div>
+        )
     }
+}
 
-    return (
-        <div className='contact' name="homeContact">
-            <form onSubmit={submitHandler}>
-                <h2>Skontaktuj się z nami</h2>
-                <div className='decoration'/>
-
-                <div className='nameAndEmail'>
-                    <div className='singleField'>
-                        <label htmlFor='contactName'>
-                            Wpisz swoje imię
-                            <input
-                                name="name"
-                                value={formData.name}
-                                onChange={(e) => inputHandler(e)}
-                            />
-                        </label>
-                    </div>
-                    <div className='singleField'>
-                        <label htmlFor='contactMail'>
-                            Wpisz swój email
-                            <input
-                                name="email"
-                                value={formData.email}
-                                onChange={(e) => inputHandler(e)}
-                            />
-                        </label>
-                    </div>
-
-                </div>
-                <label htmlFor='contactMessage'>
-                    Wpisz swoja wiadomość
-                    <input
-                        name="message"
-                        value={formData.message}
-                        onChange={(e) => inputHandler(e)}
-                    />
-                </label>
-                <div className='submit'>
-                    <input type='submit' value="Wyślij"/>
-                </div>
-            </form>
-        </div>
-    );
-};
-
-export default Contact;
+export default HomeContact;
--- src/components/Home.js
+++ src/components/Home.js
@@ -50,6 +50,7 @@ const Home = () => {
             <FourSteps/>
             <AboutUs/>
 
+
             <Contact/>
             <Footer/>
         </>
